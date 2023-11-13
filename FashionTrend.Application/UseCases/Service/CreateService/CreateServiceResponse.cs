04a16baee92d81fcfb4597169c74cffee0534805
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-11-12 21:18:52 -0300

    feat: add new properties

--- FashionTrend.Application/UseCases/Service/CreateService/CreateServiceResponse.cs
+++ FashionTrend.Application/UseCases/Service/CreateService/CreateServiceResponse.cs
@@ -1,9 +1,12 @@
 ﻿public sealed record CreateServiceResponse
-{
+{   
     public string Description { get; set; }
-    public bool Delivery { get; set; }
     public RequestType Type { get; set; }
-    public List<Material> Materials { get; set; }
+    public Product Product { get; set; }
     public List<SewingMachine> SewingMachines { get; set; }
+    public int Quantity { get; set; }
+    public double UnitPrice { get; set; }
+    public double TotalPrice { get; set; }
+    public bool Available { get; set; } 
 
 }

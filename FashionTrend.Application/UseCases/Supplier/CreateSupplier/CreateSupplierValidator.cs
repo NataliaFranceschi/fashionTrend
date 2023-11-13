d7e370241ded6f19bb633d2b5c778309cb08df0a
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-11-13 16:37:34 -0300

    feat: add password validator

--- FashionTrend.Application/UseCases/Supplier/CreateSupplier/CreateSupplierValidator.cs
+++ FashionTrend.Application/UseCases/Supplier/CreateSupplier/CreateSupplierValidator.cs
@@ -6,5 +6,6 @@ public sealed class CreateSupplierValidator : AbstractValidator<CreateSupplierRe
     {
         RuleFor(x => x.Email).NotEmpty().MaximumLength(50).EmailAddress();
         RuleFor(x => x.Name).NotEmpty().MinimumLength(3).MaximumLength(50);
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(6);
     }
 }

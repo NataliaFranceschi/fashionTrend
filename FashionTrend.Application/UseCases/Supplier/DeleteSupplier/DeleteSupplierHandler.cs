4c7f9048ad0738e0995894b58e0e4ca493fe1fe6
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-11-15 11:11:58 -0300

    refactor:renaming variable and folder

---
+++ FashionTrend.Application/UseCases/Supplier/DeleteSupplier/DeleteSupplierHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+
+
+public sealed class DeleteSupplierHandler : IRequestHandler<DeleteSupplierRequest, DeleteSupplierResponse>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ISupplierRepository _supplierRepository;
+    private readonly IMapper _mapper;
+
+    public DeleteSupplierHandler(IUnitOfWork unitOfWork,
+                             ISupplierRepository supplierRepository, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _supplierRepository = supplierRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<DeleteSupplierResponse> Handle(DeleteSupplierRequest request,
+                                                 CancellationToken cancellationToken)
+    {
+
+        var user = await _supplierRepository.Get(request.Id, cancellationToken);
+
+        if (user == null) return default;
+
+        _supplierRepository.Delete(user);
+        await _unitOfWork.Commit(cancellationToken);
+
+        return _mapper.Map<DeleteSupplierResponse>(user);
+    }
+}

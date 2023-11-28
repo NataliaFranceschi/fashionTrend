c40d150e7a5c19c5b4b3e67d9c990aa1a0a72e51
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-11-28 15:06:00 -0300

    feat: phone logic

--- FashionTrend.Application/UseCases/DraftContract/CreateDraftContractHandler.cs
+++ FashionTrend.Application/UseCases/DraftContract/CreateDraftContractHandler.cs
@@ -5,21 +5,26 @@ using MediatR;
 public class CreateDraftContractHandle : IRequestHandler<CreateDraftContractRequest, CreateDraftContractResponse>
 {
     private readonly IUnitOfWork _unitOfWork;
-
     private readonly IDraftContractRepository _repository;
-
     private readonly IMapper _mapper;
-    public CreateDraftContractHandle(IUnitOfWork unitOfWork, IDraftContractRepository draftContract, IMapper mapper )
+    private readonly ISupplierRepository _supplierRepository;
+    public CreateDraftContractHandle(IUnitOfWork unitOfWork, 
+                                    IDraftContractRepository draftContract, 
+                                    IMapper mapper,
+                                    ISupplierRepository supplierRepository)
     {
         _mapper = mapper;
         _repository = draftContract;
         _unitOfWork = unitOfWork;
+        _supplierRepository = supplierRepository;
 
     }
 
     public async Task<CreateDraftContractResponse> Handle(CreateDraftContractRequest request, CancellationToken cancellationToken)
     {
-        //validar se o usuário que esta sendo inserido no contrato existe no banco
+        var supplier = await _supplierRepository.Get(request.SupplierId, cancellationToken);
+        if (supplier == null) { throw new ArgumentException("Supplier not found"); }
+
         var draft = _mapper.Map<DraftContract>(request);
         _repository.Create(draft);
 
@@ -28,8 +33,7 @@ public class CreateDraftContractHandle : IRequestHandler<CreateDraftContractRequ
         var notificaton = new CreateNotificationHandler("AC705875abfac3bd8ffe96712d2184af3f",
             "754eb0223d1b2e287f97a26dc12d4743", "+14842763066");
 
-        notificaton.SendSMS("+5514997784271", "Olá, temos uma minuta de contrato disponível para assinatura");
-        //request.User.Telephone,
+        notificaton.SendSMS(supplier.PhoneNumber, "Olá, temos uma minuta de contrato disponível para assinatura");
 
         return _mapper.Map<CreateDraftContractResponse>(draft);
 

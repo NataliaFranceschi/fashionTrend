0777271490cd49af8740579867b2f68295bd118d
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-11-28 08:01:47 -0300

    feat: add retry

--- FashionTrend.Domain/Interfaces/IKafkaProducer.cs
+++ FashionTrend.Domain/Interfaces/IKafkaProducer.cs
@@ -2,4 +2,6 @@
 {
     Task<Message> ProduceAsync(
         string topic, string sender, string receiver, string content);
+    Task<Message> ProduceAsyncWithRetry(
+        string topic, string sender, string receiver, string content);
 }
